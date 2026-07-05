900a1b89175c80fe5f71cb2b486cf43cf85da1c1
SUDHA MAURYA <sudhamaurya9207@gmail.com>
2026-07-05 13:44:31 +0530

    Update models.py

--- student/models.py
+++ student/models.py
@@ -5,7 +5,6 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     mob = models.CharField(max_length=10)
-    message = models.TextField()
-created_at=models.DateTimeField(auto_now_add=True)
+    message = models.TextField()   created_at=models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
