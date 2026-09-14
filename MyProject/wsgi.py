286b470e5dd6a125c59eb31c2a384917302d8794
SUDHA MAURYA <sudhamaurya9207@gmail.com>
2026-09-14 19:52:09 +0530

    Update wsgi.py

--- MyProject/wsgi.py
+++ MyProject/wsgi.py
@@ -1,16 +1,20 @@
 """
 WSGI config for MyProject project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named `application`.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyProject.settings')
-
 application = get_wsgi_application()
+
+try:
+    call_command('migrate', '--noinput')
+except Exception:
+    pass
