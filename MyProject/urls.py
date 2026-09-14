170cb65639a6abf6b08365e10e2dbc76a5a8287b
SUDHA MAURYA <sudhamaurya9207@gmail.com>
2026-09-14 19:25:04 +0530

    Update urls.py

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path("student/",include("student.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
