38e8c0092b91578bded50dde65e7c45af5e0ae2f
SUDHA MAURYA <sudhamaurya9207@gmail.com>
2026-09-14 19:39:01 +0530

    Update settings.py

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -78,7 +78,8 @@ WSGI_APPLICATION = 'MyProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        
+        'NAME': '/tmp/db.sqlite3',
     }
 }
 
