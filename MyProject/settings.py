f0aac01fe7fbdaa6255a44bc12675a16609aa64b
SUDHA MAURYA <sudhamaurya9207@gmail.com>
2026-09-14 19:12:44 +0530

    Update settings.py

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIR=BASE_DIR/"static"
 STATICFILES_DIRS=[STATIC_DIR] 
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
